621ff27e82bbd36c47ed85f00bddff89a13491b4
krinkv <krikri7@protonmail.com>
2023-01-03 23:50:01 +0200

    Add subject service

--- AIS-fe/src/app/app.module.ts
+++ AIS-fe/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { StudentsHomeComponent } from './components/students-home/students-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    StudentsHomeComponent
   ],
   imports: [
     BrowserModule,
